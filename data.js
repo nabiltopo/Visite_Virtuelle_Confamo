43346a671c007f8d017480f31c684edd92ae6fe5
nabiltopo1 <nabil.boutrik1@gmail.com>
2025-02-12 22:14:13 +0100

    commit

---
+++ data.js
@@ -0,0 +1,234 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -1.4604512181146418,
+        "pitch": 0.5590404137299743,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10638278172885407,
+          "pitch": -0.10417802694043488,
+          "rotation": 3.141592653589793,
+          "target": "1-2"
+        },
+        {
+          "yaw": 1.4995043133318262,
+          "pitch": -0.06872789947923508,
+          "rotation": 3.141592653589793,
+          "target": "2-4"
+        },
+        {
+          "yaw": 2.451804561442029,
+          "pitch": -0.06425130729495265,
+          "rotation": 3.141592653589793,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.809213231882236,
+          "pitch": 0.1300984335139148,
+          "rotation": 3.141592653589793,
+          "target": "0-1"
+        },
+        {
+          "yaw": 2.1686692229762166,
+          "pitch": 0.06086738384891177,
+          "rotation": 3.141592653589793,
+          "target": "2-4"
+        },
+        {
+          "yaw": 2.797287457121185,
+          "pitch": 0.015681593178575426,
+          "rotation": 3.141592653589793,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.617386251367524,
+          "pitch": 0.02788880519060477,
+          "rotation": 3.141592653589793,
+          "target": "0-1"
+        },
+        {
+          "yaw": 3.118242584195105,
+          "pitch": -0.09536326180325005,
+          "rotation": 3.141592653589793,
+          "target": "1-2"
+        },
+        {
+          "yaw": 1.3663236471645774,
+          "pitch": 0.3070211660753337,
+          "rotation": 3.141592653589793,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08239353723256215,
+          "pitch": 0.10851763267449144,
+          "rotation": 3.141592653589793,
+          "target": "0-1"
+        },
+        {
+          "yaw": 0.7073258287531967,
+          "pitch": -0.031652799232626094,
+          "rotation": 3.141592653589793,
+          "target": "1-2"
+        },
+        {
+          "yaw": 1.364358751337992,
+          "pitch": 0.04292381029171288,
+          "rotation": 3.141592653589793,
+          "target": "2-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15667895669325382,
+          "pitch": 0.36658883555666044,
+          "title": "Carrière Confamo",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Carrière Confamo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
